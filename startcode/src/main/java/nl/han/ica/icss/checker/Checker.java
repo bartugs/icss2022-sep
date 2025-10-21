d2bd8b5881be76de0889a8ff83e4fa5b41bbce5b
bartugs <117641579+bartugs@users.noreply.github.com>
2025-10-21 11:01:48 +0200

    Update Checker.java

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,21 +1,41 @@
 package nl.han.ica.icss.checker;
 
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
-
+import java.util.LinkedList;
 
 
 public class Checker {
 
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    private LinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
+        checkStylesheet(ast.root);
         // variableTypes = new HANLinkedList<>();
 
     }
 
-    
+    private void checkStylesheet(Stylesheet sheet) {
+        checkStylerule((Stylerule) sheet.getChildren().get(0));
+    }
+
+    private void checkStylerule(Stylerule rule) {
+        for (ASTNode child : rule.getChildren()) {
+            if (child instanceof Declaration) {
+                checkDeclaration((Declaration) child);
+            }
+        }
+    }
+
+    private void checkDeclaration(Declaration declaration) {
+        if(declaration.property.name.equals("width")){
+            if(declaration.expression instanceof ColorLiteral) {
+                declaration.setError("Property 'width' : color not allowed");
+            }
+        }
+    }
 }
